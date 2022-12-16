569d1358a44a7713d422415764cbfd5d5df41b54
GledsonFlorencio94 <gledsonflorencio@gmail.com>
2022-12-16 14:45:36 -0300

    Criando projeto

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button`
+    width: 100%;
+    height: 42px;
+    background-color: #0000FF;
+;
+    color: #FFF;
+
+    border: 1px solid #0000FF;
+;
+    border-radius: 21px;
+
+    &:hover {
+        opacity: 0.6;
+        cursor:pointer;
+    }
+`
